abacf2466a7b9e27262a9ec96f1b613e86856f75
승엽 <tmdduqflfl@naver.com>
2024-01-07 10:00:00 +0900

    study

--- src/LV0/UnlikeOdd.java
+++ src/LV0/UnlikeOdd.java
@@ -5,11 +5,15 @@ public class UnlikeOdd {
         int[] answer = new int[(n + 1) / 2]; // n 이하의 홀수가 들어갈 배열의 크기
 
         int j = 0; // 배열의 인덱스
+        extracted(n, answer, j);
+
+        return answer;
+    }
+
+    private static void extracted(final int n, final int[] answer, int j) {
         for (int i = 1; i <= n; i += 2) {
             answer[j] = i;
             j++; // 다음 인덱스로 이동
         }
-
-        return answer;
     }
 }
