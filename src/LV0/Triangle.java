f0fd5a2fe2d30ad900b072370b82ebbce7d9aa32
승엽 <tmdduqflfl@naver.com>
2024-01-08 10:00:00 +0900

    study

--- src/LV0/Triangle.java
+++ src/LV0/Triangle.java
@@ -6,6 +6,11 @@ public class Triangle {
     public int solution(int[] sides) {
         Arrays.sort(sides);
         int answer = 0;
+        return extracted(sides);
+    }
+
+    private static int extracted(final int[] sides) {
+        int answer;
         if (sides[2] < sides[0] + sides[1]) {
             answer = 1;
         } else {
