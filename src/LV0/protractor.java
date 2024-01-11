3662951fa163a21f9fa3122d85763621c94c25e9
승엽 <tmdduqflfl@naver.com>
2024-01-11 10:00:00 +0900

    study

--- src/LV0/protractor.java
+++ src/LV0/protractor.java
@@ -4,20 +4,25 @@ public class protractor {
     public int solution(int angle) {
         int answer=0;
         if( angle>0 && angle<=180){
-            if (angle <90){
-                answer=1;
-            }
-            else if (angle==90){
-                answer=2;
-            }
-            else if (angle>90){
-                answer =3;
-            }
-            else if (angle==180){
-                answer=4;
-            }
+            answer = getAnswer(angle, answer);
         }
 
+        return answer;
+    }
+
+    private static int getAnswer(final int angle, int answer) {
+        if (angle <90){
+            answer =1;
+        }
+        else if (angle ==90){
+            answer =2;
+        }
+        else if (angle >90){
+            answer =3;
+        }
+        else if (angle ==180){
+            answer =4;
+        }
         return answer;
     }
 }
