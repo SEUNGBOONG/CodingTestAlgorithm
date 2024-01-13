a72f3acaaf872abca5cd092fd4e4cb2058c24717
승엽 <tmdduqflfl@naver.com>
2024-01-13 10:00:00 +0900

    study

--- src/LV0/mutipleArr.java
+++ src/LV0/mutipleArr.java
@@ -4,10 +4,15 @@ public class mutipleArr {
     public int[] solution(int[] numbers) {
         int i;
         int[] answer=new int[numbers.length]; //배열 초기화 하는것을 기억하자
-        for(i=0;i<numbers.length; i++){
+        extracted(numbers, answer);
+        return answer;
+    }
+
+    private static void extracted(final int[] numbers, final int[] answer) {
+        int i;
+        for(i=0; i< numbers.length; i++){
             answer[i] = numbers[i]*2;
         }
-        return answer;
     }
 
 }
