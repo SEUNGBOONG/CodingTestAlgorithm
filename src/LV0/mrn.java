bc401169ffb6d05e58ccd69d1ee039ba37e27929
승엽 <tmdduqflfl@naver.com>
2024-01-14 10:00:00 +0900

    study

--- src/LV0/mrn.java
+++ src/LV0/mrn.java
@@ -4,13 +4,18 @@ package LV0;
 //프로그래머스에 적응 하는 중 생각보다 쉽게 풀었음
 public class mrn {
     public String solution(String rny_string) {
+        String answer = getString(rny_string);
+        if (answer != null) return answer;
+
+        return rny_string;
+    }
+
+    private static String getString(final String rny_string) {
         if(rny_string.contains("m")){
             String answer = rny_string.replace("m","rn");
             return answer;
         }
-        //replace, contains 기억하고 쓰자
-
-        return rny_string;
+        return null;
     }
     //
 
