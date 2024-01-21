5f4edba3e4cbddff95347f7e82111a22a125379f
승엽 <tmdduqflfl@naver.com>
2024-01-21 10:00:00 +0900

    study

--- src/LV0/Letter.java
+++ src/LV0/Letter.java
@@ -8,7 +8,7 @@ public class Letter {
         // 각 문자의 대소문자를 변환
         for (int i = 0; i < chars.length; i++) {
             if (Character.isUpperCase(chars[i])) { // 대문자인 경우
-                chars[i] = Character.toLowerCase(chars[i]); // 대문자를 소문자로 변환
+                chars[i] = Character.toLowerCase(chars[i]);
             } else if (Character.isLowerCase(chars[i])) {
                 chars[i] = Character.toUpperCase(chars[i]);
             }
