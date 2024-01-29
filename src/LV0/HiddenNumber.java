31ef3938146f99b75727ce765cc2748d0ca319af
승엽 <tmdduqflfl@naver.com>
2024-01-29 10:00:00 +0900

    study

--- src/LV0/HiddenNumber.java
+++ src/LV0/HiddenNumber.java
@@ -2,6 +2,11 @@ package LV0;
 
 public class HiddenNumber {
     public int solution(String my_string) {
+        int answer = getAnswer(my_string);
+        return answer;
+    }
+
+    private static int getAnswer(final String my_string) {
         int answer = 0;
         for (int i = 0; i < my_string.length(); i++) {
             char currentChar = my_string.charAt(i);
