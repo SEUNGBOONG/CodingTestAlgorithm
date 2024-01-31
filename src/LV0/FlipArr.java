88cabfdcbff4461a0be8e154ed969842d7c940cc
승엽 <tmdduqflfl@naver.com>
2024-01-31 10:00:00 +0900

    study

--- src/LV0/FlipArr.java
+++ src/LV0/FlipArr.java
@@ -3,10 +3,14 @@ package LV0;
 public class FlipArr {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
+        extracted(num_list, answer);
+        return answer;
+    }
+
+    private static void extracted(final int[] num_list, final int[] answer) {
         for(int i = num_list.length - 1; i >= 0; i--){
             answer[num_list.length - 1 - i] = num_list[i];
         }
-        return answer;
     }
 
 }
