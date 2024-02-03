e60c15a1d69dae0e550c98b80eac05d28fe31c6b
승엽 <tmdduqflfl@naver.com>
2024-02-03 10:00:00 +0900

    study

--- src/LV0/evenSum.java
+++ src/LV0/evenSum.java
@@ -4,9 +4,16 @@ public class evenSum {
     public int solution(int n) {
         int answer = 0;
         int i;
-        for(i=0;i<=n; i++){
+
+        answer = getAnswer(n, answer);
+        return answer;
+    }
+
+    private static int getAnswer(final int n, int answer) {
+        int i;
+        for(i=0; i<= n; i++){
            if(i%2==0) {
-               answer+=i;
+               answer +=i;
            }
         }
         return answer;
