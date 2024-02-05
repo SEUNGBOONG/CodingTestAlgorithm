7785480e909759f60d1ac4536204953a41088072
승엽 <tmdduqflfl@naver.com>
2024-02-05 10:00:00 +0900

    study

--- src/LV0/dupliArr.java
+++ src/LV0/dupliArr.java
@@ -7,11 +7,17 @@ public class dupliArr {
         int answer = 0;
         int count=0;
         int i;
-        for(i=0; i<array.length;i++){
-            if(array[i] ==n){
+        answer = getAnswer(array, n, count, answer);
+        return answer;
+    }
+
+    private static int getAnswer(final int[] array, final int n, int count, int answer) {
+        int i;
+        for(i=0; i< array.length; i++){
+            if(array[i] == n){
                 count++;
             }
-            answer=count;
+            answer = count;
         }
         return answer;
     }
