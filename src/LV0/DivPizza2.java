8bd4bfb3fa3bf0b920d5eb948ae89e5f30fed5ad
승엽 <tmdduqflfl@naver.com>
2024-02-07 10:00:00 +0900

    study

--- src/LV0/DivPizza2.java
+++ src/LV0/DivPizza2.java
@@ -3,11 +3,17 @@ package LV0;
 public class DivPizza2 {
     public int solution(int slice, int n) {
         int answer = 0;
-        if(n%slice==0){
-            answer=(n/slice);
+        answer = getAnswer(slice, n);
+        return answer;
+    }
+
+    private static int getAnswer(final int slice, final int n) {
+        int answer;
+        if(n % slice ==0){
+            answer=(n / slice);
         }
         else{
-            answer=(n/slice)+1;
+            answer=(n / slice)+1;
         }
         return answer;
     }
