615a3d8249535a5cadcb4c8ffd24bfaed871a156
승엽 <tmdduqflfl@naver.com>
2024-02-08 10:00:00 +0900

    study

--- src/LV0/DivPizza2.java
+++ src/LV0/DivPizza2.java
@@ -3,7 +3,6 @@ package LV0;
 public class DivPizza2 {
     public int solution(int slice, int n) {
         int answer = 0;
-        //n=14; slice=5
         if(n%slice==0){
             answer=(n/slice);
         }
