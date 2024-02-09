e831cf4970dcb6e8b554196977254abb8213a43f
승엽 <tmdduqflfl@naver.com>
2024-02-09 10:00:00 +0900

    study

--- src/LV0/DivPizza.java
+++ src/LV0/DivPizza.java
@@ -4,7 +4,6 @@ public class DivPizza {
     public int solution(int n) {
         int answer;
         int PIZZA_PIECE = 7;
-
         if (n % PIZZA_PIECE == 0) {
             answer = n / PIZZA_PIECE; // 피자 조각의 수가 7의 배수인 경우
         } else {
