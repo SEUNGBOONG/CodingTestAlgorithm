d37fd52279a05b8f1be3963ead2c906030b5e002
승엽 <tmdduqflfl@naver.com>
2024-02-14 10:00:00 +0900

    study

--- src/LV0/ClothesDiscount.java
+++ src/LV0/ClothesDiscount.java
@@ -3,13 +3,11 @@ package LV0;
 public class ClothesDiscount {
     public int solution(int price) {
         int answer = 0;
-        if(price >= 100000 && price < 300000){
+        if (price >= 100000 && price < 300000) {
             answer = (int) (price * 0.95);
-        }
-        else if(price >= 300000 && price < 500000){
-            answer = (int)(price * 0.9);
-        }
-        else if(price >= 500000){
+        } else if (price >= 300000 && price < 500000) {
+            answer = (int) (price * 0.9);
+        } else if (price >= 500000) {
             answer = (int) (price * 0.8);
         }
         return answer;
