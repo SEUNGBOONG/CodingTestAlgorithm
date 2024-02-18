3ef2158795f8ac79f55cfdcded3373fc51deaf45
승엽 <tmdduqflfl@naver.com>
2024-02-18 10:00:00 +0900

    study

--- src/LV0/avgArr.java
+++ src/LV0/avgArr.java
@@ -3,11 +3,11 @@ package LV0;
 public class avgArr {
     public double solution(int[] numbers) {
         int i;
-        double sum=0; //처음에는 여기서 int로 선언해서 평균 값 구할 떄 테스트가 전부 다 맞진 않았다.
-        for(i=0; i<numbers.length; i++){
-            sum +=numbers[i];
+        double sum = 0; //처음에는 여기서 int로 선언해서 평균 값 구할 떄 테스트가 전부 다 맞진 않았다.
+        for (i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
         }
-        double answer = (sum/ numbers.length);
+        double answer = (sum / numbers.length);
 
         return answer;
     }
