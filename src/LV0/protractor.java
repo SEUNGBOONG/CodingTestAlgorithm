6d7fa015ed82edb07d9974b0370d2eb4704fc3ec
승엽 <tmdduqflfl@naver.com>
2024-05-04 10:14:12 +0900

    5월 4일 코테

---
+++ src/LV0/protractor.java
@@ -0,0 +1,23 @@
+package LV0;
+
+public class protractor {
+    public int solution(int angle) {
+        int answer=0;
+        if( angle>0 && angle<=180){
+            if (angle <90){
+                answer=1;
+            }
+            else if (angle==90){
+                answer=2;
+            }
+            else if (angle>90){
+                answer =3;
+            }
+            else if (angle==180){
+                answer=4;
+            }
+        }
+
+        return answer;
+    }
+}
