c9e3c75044f0ee94b77fac9ab872ca055fba3cfa
승엽 <tmdduqflfl@naver.com>
2024-05-05 12:48:08 +0900

    5월 5일 코테

---
+++ src/LV0/duplicationArr.java
@@ -0,0 +1,8 @@
+package LV0;
+
+public class duplicationArr {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        return answer;
+    }
+}
--- src/LV0/mutipleArr.java
+++ src/LV0/mutipleArr.java
@@ -3,7 +3,7 @@ package LV0;
 public class mutipleArr {
     public int[] solution(int[] numbers) {
         int i;
-        int[] answer={};
+        int[] answer=new int[numbers.length]; //배열 초기화 하는것을 기억하자
         for(i=0;i<numbers.length; i++){
             answer[i] = numbers[i]*2;
         }
