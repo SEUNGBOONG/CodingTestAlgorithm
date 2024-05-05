ce8d165fddf24d90031191b10670d4c521b92787
승엽 <tmdduqflfl@naver.com>
2024-05-05 16:36:43 +0900

    5월 5일 코테 하드코딩으로만 생각하지말자

---
+++ src/LV0/DivPizza.java
@@ -0,0 +1,16 @@
+package LV0;
+
+public class DivPizza {
+    public int solution(int n) {
+        int answer;
+        int PIZZA_PIECE=7;
+
+        if (n % PIZZA_PIECE == 0) {
+            answer = n / PIZZA_PIECE; // 피자 조각의 수가 7의 배수인 경우
+        } else {
+            answer = n / PIZZA_PIECE + 1; // 피자 조각의 수가 7의 배수가 아닌 경우
+        }
+
+        return answer;
+    }
+}
---
+++ src/LV0/DivPizza2.java
@@ -0,0 +1,15 @@
+package LV0;
+
+public class DivPizza2 {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        //n=14; slice=5
+        if(n%slice==0){
+            answer=(n/slice);
+        }
+        else{
+            answer=(n/slice)+1;
+        }
+        return answer;
+    }
+}
