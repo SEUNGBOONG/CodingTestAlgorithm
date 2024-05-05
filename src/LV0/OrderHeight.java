dc9a79159ee3d075853ddd4c0bdf5cf648656c86
승엽 <tmdduqflfl@naver.com>
2024-05-05 15:29:38 +0900

    5월 5일 코테

---
+++ src/LV0/OrderHeight.java
@@ -0,0 +1,15 @@
+package LV0;
+
+public class OrderHeight {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        int i;
+
+        for(i=0;i<array.length;i++){
+            if(array[i]>height){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
