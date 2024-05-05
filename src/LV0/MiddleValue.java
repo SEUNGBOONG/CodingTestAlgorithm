5660d47b49ee09aa967c8906f7b3eca2532a6bbf
승엽 <tmdduqflfl@naver.com>
2024-05-05 16:07:14 +0900

    5월 5일 코테

--- src/LV0/MiddleValue.java
+++ src/LV0/MiddleValue.java
@@ -1,13 +1,26 @@
 package LV0;
 
+
+import java.util.Arrays;
+
 public class MiddleValue {
     public int solution(int[] array) {
-        int middleIndex = array.length % 2;
-        int answer = 0;
-        if (middleIndex == 1){
-            middleIndex++;
+        // 배열을 오름차순으로 정렬
+        Arrays.sort(array);
+
+        // 중앙값을 저장할 변수
+        int median;
+
+        // 배열의 길이가 홀수인 경우
+        if (array.length % 2 == 1) {
+            median = array[array.length / 2]; // 중앙값은 중간 위치의 요소
         }
-        answer=array[middleIndex];
-        return answer;
+        // 배열의 길이가 짝수인 경우
+        else {
+            int middleIndex = array.length / 2;
+            median = (array[middleIndex - 1] + array[middleIndex]) / 2; // 중앙 두 값의 평균
+        }
+
+        return median;
     }
 }
