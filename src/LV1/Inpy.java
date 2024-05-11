d747f06db86736e6ca0ac97d40c57cbc2c432578
승엽 <tmdduqflfl@naver.com>
2024-05-11 15:38:06 +0900

    5월 11일 코테

--- src/LV1/Inpy.java
+++ src/LV1/Inpy.java
@@ -1,20 +1,24 @@
 package LV1;
 
 public class Inpy {
-    boolean solution(String s) {
-        String strp = String.valueOf(countChar(s, 'p'));
-        String stry = String.valueOf(countChar(s, 'y'));
-        boolean answer = false;
-        if (strp.equals(stry)) {
-            answer = true;
+    public boolean solution(String s) {
+        // 대문자로 변환하여 비교하기 위해 모두 소문자로 변경
+        s = s.toLowerCase();
+
+        // 'p'와 'y'의 개수 카운트
+        int countP = 0;
+        int countY = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'p') {
+                countP++;
+            } else if (ch == 'y') {
+                countY++;
+            }
         }
 
-        return answer;
-    }
-
-    public static long countChar(String s, char ch) {
-        return s.chars()
-                .filter(c -> c == ch)
-                .count();
+        // 'p'와 'y'의 개수 비교
+        return countP == countY;
     }
 }
