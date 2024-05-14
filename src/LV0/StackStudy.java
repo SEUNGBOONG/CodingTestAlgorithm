218bcbb87500e5e10f9b58783423952b7f52e626
승엽 <tmdduqflfl@naver.com>
2024-05-14 13:32:58 +0900

    5월 15일 코테 - Stack 공부

--- src/LV0/StackStudy.java
+++ src/LV0/StackStudy.java
@@ -9,6 +9,19 @@ public class StackStudy {
         stack.push(2);
         stack.push(3);
         System.out.println(stack);
+
+        System.out.println(stack.pop());
+        //LIFO 형식이다보니 가장 마지막에 들어간 3이빠져나간다.
+
+        System.out.println(stack.peek());
+        //가장 마지막 값 확인하고 제거하지는 않는다.
+        if(!stack.empty()){
+            stack.pop();
+        }
+        //스택이 비어있는지 확인
+
+        stack.add(0,4);
+
     }
 
 
