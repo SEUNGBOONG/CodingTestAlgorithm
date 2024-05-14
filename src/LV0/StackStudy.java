86bfeba85197502aa975288fbd2b590f071b2539
승엽 <tmdduqflfl@naver.com>
2024-05-14 13:27:31 +0900

    5월 15일 코테 - Stack 공부

---
+++ src/LV0/StackStudy.java
@@ -0,0 +1,15 @@
+package LV0;
+
+
+import java.util.Stack;
+public class StackStudy {
+    public static void main(String[] args) {
+        Stack<Number> stack=new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack);
+    }
+
+
+}
