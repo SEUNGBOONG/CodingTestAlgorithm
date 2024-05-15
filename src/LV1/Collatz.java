87912b4bfb640f567ef88035c938b71d34db9ec6
승엽 <tmdduqflfl@naver.com>
2024-05-15 15:53:41 +0900

    5월 16일 코테 -

---
+++ src/LV1/Collatz.java
@@ -0,0 +1,10 @@
+package LV1;
+
+public class Collatz {
+    public int solution(int num) {
+        int answer;
+        while ()
+            num=(num%2==0) ? (num/2) : (num*3)+1;
+
+        return answer;
+}
---
+++ src/LV1/DuplicationRemove.java
@@ -0,0 +1,12 @@
+package LV1;
+
+public class DuplicationRemove {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
