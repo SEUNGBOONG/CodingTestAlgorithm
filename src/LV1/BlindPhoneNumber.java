63ec99255d2ebfbeacdd69232e5495c9e9ebab57
승엽 <tmdduqflfl@naver.com>
2024-05-16 16:44:44 +0900

    5월 16일 코테 -문제풀이

---
+++ src/LV1/BlindPhoneNumber.java
@@ -0,0 +1,20 @@
+package LV1;
+
+
+public class BlindPhoneNumber {
+    public String solution(String phone_number) {
+        String answer = "";
+        String[] num = phone_number.split("");
+
+        int len = num.length;
+
+        for (int i = 0; i < len; i++) {
+            if (i < len - 4)
+                answer += "*";
+            else
+                answer += num[i];
+        }
+
+        return answer;
+    }
+}
