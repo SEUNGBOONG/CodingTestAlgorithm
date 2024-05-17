e95be903e6cfb4c00c1d7d57f781c4e27e5ab947
승엽 <tmdduqflfl@naver.com>
2024-05-17 17:45:06 +0900

    5월 17일 코테

--- src/LV1/DuplicationRemove.java
+++ src/LV1/DuplicationRemove.java
@@ -4,7 +4,6 @@ public class DuplicationRemove {
     public int[] solution(int []arr) {
         int[] answer = {};
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("Hello Java");
 
         return answer;
