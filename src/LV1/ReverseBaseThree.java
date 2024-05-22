7f372e7a5fe321682231dd8f646f412d11b18ddf
승엽 <tmdduqflfl@naver.com>
2024-05-22 21:37:19 +0900

    5월 22일 코테 - Stack 공부하기

---
+++ src/LV1/ReverseBaseThree.java
@@ -0,0 +1,11 @@
+package LV1;
+
+public class ReverseBaseThree {
+    public int solution(int n) {
+        int answer = 0;
+        if(n%3==0){
+
+        }
+        return answer;
+    }
+}
--- src/LV2/스택연습문제2.java
+++ src/LV2/스택연습문제2.java
@@ -7,8 +7,6 @@ public class 스택연습문제2 {
 //    boolean solution(String s) {
 //        // stack으로 문제 푼다
 //        Stack<Character> branket = new Stack<>();
-//        List<String> a = List.of(s.split(""));
-//        branket.push(a[0])
 //        boolean answer = false;
 //
 //        // 2. 언제 정답이 true일지 규칙을 찾는다.
@@ -17,12 +15,8 @@ public class 스택연습문제2 {
 //        // 3. 문자열 하나하나를 뜯어서 Stack에 넣으면서 비교를 한다
 //        for (int i = 0; i < s.length(); i++) {
 //            char c = s.charAt(i);
-//            if(branket.pop()==branket.pop()){
-//
-//            }
-//
 //        }
-
+//
 //        return branket.isEmpty();
         public boolean solution(String s) {
             Stack<Character> stack = new Stack<>();
