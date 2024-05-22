d9efd0232520c22ff3204e6b49b32f44784fb9cf
승엽 <tmdduqflfl@naver.com>
2024-05-22 21:35:23 +0900

    5월 22일 코테 - Stack 공부하기

--- src/LV2/스택연습문제2.java
+++ src/LV2/스택연습문제2.java
@@ -1,20 +1,43 @@
 package LV2;
 
+import java.util.List;
 import java.util.Stack;
 
 public class 스택연습문제2 {
-    boolean solution(String s) {
-        // stack으로 문제 푼다
-        Stack<Character> branket = new Stack<>();
-        boolean answer = false;
+//    boolean solution(String s) {
+//        // stack으로 문제 푼다
+//        Stack<Character> branket = new Stack<>();
+//        List<String> a = List.of(s.split(""));
+//        branket.push(a[0])
+//        boolean answer = false;
+//
+//        // 2. 언제 정답이 true일지 규칙을 찾는다.
+//
+//
+//        // 3. 문자열 하나하나를 뜯어서 Stack에 넣으면서 비교를 한다
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            if(branket.pop()==branket.pop()){
+//
+//            }
+//
+//        }
 
-        // 2. 언제 정답이 true일지 규칙을 찾는다.
+//        return branket.isEmpty();
+        public boolean solution(String s) {
+            Stack<Character> stack = new Stack<>();
 
-        // 3. 문자열 하나하나를 뜯어서 Stack에 넣으면서 비교를 한다
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-        }
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(') {
+                    stack.push(c);
+                } else {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                }
+            }
 
-        return branket.isEmpty();
+            return stack.isEmpty();
     }
 }
