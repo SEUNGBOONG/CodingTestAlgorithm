fe9d89532e9c200cf3db57587a5b017c8b863fe0
승엽 <tmdduqflfl@naver.com>
2024-05-24 16:00:17 +0900

    5월 22일 코테 - 흠 다시 풀어봐야겠다

---
+++ src/LV1/삼총사.java
@@ -0,0 +1,18 @@
+package LV1;
+
+public class 삼총사 {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer += 1;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
