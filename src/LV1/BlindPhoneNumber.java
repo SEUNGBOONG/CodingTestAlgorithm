4bca1a637f11ccf0fec19525832ecb5e19eb832f
승엽 <tmdduqflfl@naver.com>
2024-05-29 11:23:30 +0900

    5월 28일 코테 -

--- src/LV1/BlindPhoneNumber.java
+++ src/LV1/BlindPhoneNumber.java
@@ -3,17 +3,17 @@ package LV1;
 
 public class BlindPhoneNumber {
     public String solution(String phone_number) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         String[] num = phone_number.split("");
 
         int len = num.length;
 
         for (int i = 0; i < len; i++) {
             if (i < len - 4)
-                answer += "*";
+                answer.append("*");
             else
-                answer += num[i];
+                answer.append(num[i]);
         }
-        return answer;
+        return answer.toString();
     }
 }
