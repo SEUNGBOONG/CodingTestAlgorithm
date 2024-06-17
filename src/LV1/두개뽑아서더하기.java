c41000cb984bf098065b5787aa79b5cedb5e1e00
승엽 <tmdduqflfl@naver.com>
2024-06-17 20:38:54 +0900

    6월 17일 코테

--- src/LV1/두개뽑아서더하기.java
+++ src/LV1/두개뽑아서더하기.java
@@ -1,25 +1,30 @@
 package LV1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class 두개뽑아서더하기 {
 
-    public List<Integer> solution(List<Integer> numbers) {
-        Set<Integer> resultSet = new HashSet<>();
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> temp = new ArrayList<Integer>();
 
-        for (int i = 0; i < numbers.size(); i++) {
-            for (int j = i + 1; j < numbers.size(); j++) {
-                int sum = numbers.get(i) + numbers.get(j);
-                resultSet.add(sum);
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                int sum = numbers[i]+numbers[j];
+                if(!temp.contains(sum))
+                    temp.add(sum);
             }
         }
-
-        List<Integer> resultList = new ArrayList<>(resultSet);
-        Collections.sort(resultList);
-        return resultList;
+        Collections.sort(temp);
+        int[] answer = new int[temp.size()];
+        for(int i=0; i<temp.size(); i++){
+            answer[i] = temp.get(i);
+        }
+        return answer;
     }
+
 }
