d82c0924eace6a902cbeed1cdafca7f2120b28ed
승엽 <tmdduqflfl@naver.com>
2024-06-17 20:39:02 +0900

    6월 18일 코테 다시풀어볼문제

---
+++ src/LV1/K번째수.java
@@ -0,0 +1,20 @@
+package LV1;
+
+import java.util.Arrays;
+
+public class K번째수 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i =0, size=commands.length; i<size; i++){
+            int start = commands[i][0]-1;
+            int end = commands[i][1];
+            int k = commands[i][2]-1;
+            int[] temp = Arrays.copyOfRange(array,start,end);
+
+            Arrays.sort(temp);
+            answer[i] = temp[k];
+        }
+        return answer;
+    }
+}
