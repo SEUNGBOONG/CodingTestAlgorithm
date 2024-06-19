9df4923994bc7e45a0d7ec1ece38f848c1931998
승엽 <tmdduqflfl@naver.com>
2024-06-19 17:44:40 +0900

    6월 19일 코테 다시풀어보자

---
+++ src/LV1/명예의전당.java
@@ -0,0 +1,24 @@
+package LV1;
+
+import java.util.*;
+
+public class 명예의전당 {
+    public int[] solution(int k, int[] score) {
+        int answer[] = new int[score.length];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            if (list.size() < k)
+                list.add(score[i]);
+            else {
+                if (list.get(0) < score[i]) {
+                    list.set(0, score[i]);
+                }
+            }
+            Collections.sort(list);
+            answer[i] = list.get(0);
+        }
+
+        return answer;
+    }
+}
