bb82cf0b8edfc537ab7b4db3581236a949e80b57
승엽 <tmdduqflfl@naver.com>
2024-06-25 16:00:09 +0900

    6/25 코테 테스트는 통과했으나 결과는 틀린

--- src/LV1/소수찾기.java
+++ src/LV1/소수찾기.java
@@ -4,13 +4,27 @@ import java.util.List;
 
 public class 소수찾기 {
     public int solution(int n) {
-        int answer = 2;
         int i;
-        for (i = 2; i <= n; i++) {
-            if (i % 2 != 0 && i % 3 != 0) {
-                answer++;
+        int answer = 0;
+
+        if (n >= 3) {
+            answer = 2;
+            for (i = 4; i <= n; i++) {
+
+                if (i % 2 != 0 && i % 3 != 0) {
+                    answer++;
+                }
             }
         }
+        else if (n <= 3) {
+            answer = 1;
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        소수찾기 s = new 소수찾기();
+        System.out.println(s.solution(2));
     }
 }
