43b2ba4c9a9342f51e7a1ffa57bc55ab136d52f0
승엽 <tmdduqflfl@naver.com>
2024-06-27 18:14:18 +0900

    6/27 코딩테스트 다시 0단계 부터 차근차근 쌓기 재도전

--- src/LV0/N의배수구하기.java
+++ src/LV0/N의배수구하기.java
@@ -7,11 +7,17 @@ public class N의배수구하기 {
     public List<Integer> solution(int n, int[] numlist) {
         List<Integer> answer = new ArrayList<>();
 
-        for (int i = 1; i < numlist.length; i++) {
+        for (int i = 0; i < numlist.length; i++) {
             if (numlist[i] % n == 0) {
                 answer.add(numlist[i]);
             }
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        N의배수구하기 n = new N의배수구하기();
+
+        System.out.println(n.solution(5, new int[]{1,9,3,10,13,5}));
     }
 }
