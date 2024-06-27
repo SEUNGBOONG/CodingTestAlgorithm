3ac8b61dc65c2f6c0016af7f4aadeaad346b477a
승엽 <tmdduqflfl@naver.com>
2024-06-27 19:29:02 +0900

    6/27 n의배수

---
+++ src/LV0/n의배수.java
@@ -0,0 +1,15 @@
+package LV0;
+
+public class n의배수 {
+    public int solution(int num, int n) {
+        int answer = 0;
+        if (num % n == 0) {
+            answer = 1;
+        }
+        else {
+            answer=0;
+        }
+        return answer;
+    }
+}
+
