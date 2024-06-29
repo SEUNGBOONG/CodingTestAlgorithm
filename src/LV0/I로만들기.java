ce57386299190d23c7bba328865099acd035b510
승엽 <tmdduqflfl@naver.com>
2024-06-29 18:44:37 +0900

    6/28 코테

---
+++ src/LV0/I로만들기.java
@@ -0,0 +1,7 @@
+package LV0;
+
+public class I로만들기 {
+    public String solution(String myString) {
+        return myString.replaceAll("[a-i]", "i");
+    }
+}
---
+++ src/LV0/대문자로바꾸기.java
@@ -0,0 +1,7 @@
+package LV0;
+
+public class 대문자로바꾸기 {
+    public String solution(String myString) {
+        return myString.toUpperCase();
+    }
+}
