d33c9596dd430d39785c93e755e2907e03a2616a
승엽 <tmdduqflfl@naver.com>
2024-06-30 15:26:19 +0900

    6/30 문자열 곱하기

---
+++ src/LV0/문자열곱하기.java
@@ -0,0 +1,11 @@
+package LV0;
+
+public class 문자열곱하기 {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for (int i = 0; i < k; i++) {
+            answer = my_string;
+        }
+        return answer;
+    }
+}
