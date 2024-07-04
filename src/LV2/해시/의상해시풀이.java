1aeeb1ea643cfae095fba826360114bfa5f30825
승엽 <tmdduqflfl@naver.com>
2024-07-04 20:45:12 +0900

    7/4 해시로 해결한 클래스

---
+++ src/LV2/해시/의상해시풀이.java
@@ -0,0 +1,21 @@
+package LV2.해시;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 의상해시풀이 {
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> kinds = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            kinds.put(clothes[i][1], kinds.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+        for (int i : kinds.values()) {
+            answer *= i + 1;
+        }
+
+        // 아무것도 입지 않는 경우 제외
+        return answer - 1;
+    }
+}
