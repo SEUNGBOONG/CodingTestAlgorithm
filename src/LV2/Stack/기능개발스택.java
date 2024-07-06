56342da1efb9c4e0124756ef88c7e869d818fb1e
승엽 <tmdduqflfl@naver.com>
2024-07-06 16:55:55 +0900

    7/5 큐로 문제푸는건데 아직 솔직하게 어렵다 다시풀어보자

---
+++ src/LV2/Stack/기능개발스택.java
@@ -0,0 +1,34 @@
+package LV2.Stack;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 기능개발스택 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> workDate = new LinkedList<>();
+        ArrayList<Integer> release = new ArrayList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            //  반올림 하는 ceil
+            workDate.add((int) Math.ceil((double) (100 - progresses[i]) / speeds[i]));
+        }
+        //남은기간 설정완료
+        int front = workDate.poll();
+        int count = 1;
+        while (!workDate.isEmpty()) {
+            if (front < workDate.peek()) {
+                release.add(count);
+                count = 1;
+                front = workDate.poll();
+            } else {
+                count++;
+                workDate.poll();
+            }
+        }
+        release.add(count);
+
+        return release.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+//[93, 30, 55]	[1, 30, 5]	[2, 1]
