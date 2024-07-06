756c86ba0e3d7b6538dba93e41684cb78b49e7c0
승엽 <tmdduqflfl@naver.com>
2024-07-06 17:03:04 +0900

    7/5 스택 공부

---
+++ src/기능별/스택/같은숫자는싫어.java
@@ -0,0 +1,13 @@
+package 기능별.스택;
+
+public class 같은숫자는싫어 {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
+//[1,1,3,3,0,1,1]	[1,3,0,1]
