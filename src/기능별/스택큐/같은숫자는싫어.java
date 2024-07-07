0ad12772c0c50396035d5e92f46a0bcc9967f8ef
승엽 <tmdduqflfl@naver.com>
2024-07-07 16:39:30 +0900

    7/7 이동

---
+++ src/기능별/스택큐/같은숫자는싫어.java
@@ -0,0 +1,16 @@
+package 기능별.스택큐;
+
+import java.util.Stack;
+
+public class 같은숫자는싫어 {
+    public Stack<Integer> solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        for (int num : arr) {
+            if (stack.isEmpty() || stack.peek() != num) {
+                stack.push(num);
+            }
+        }
+        return stack;
+    }
+}
+//[1,1,3,3,0,1,1]	[1,3,0,1]
