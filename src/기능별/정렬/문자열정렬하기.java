4e9583de7edca9c5ab9d8b02b16b83defd5b3087
승엽 <tmdduqflfl@naver.com>
2024-08-04 15:33:19 +0900

    8/4 코테

---
+++ src/기능별/정렬/문자열정렬하기.java
@@ -0,0 +1,29 @@
+package 기능별.정렬;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.Collections.sort;
+
+public class 문자열정렬하기 {
+
+    public List<Integer> solution(String my_string) {
+
+        List<String> s1 = List.of(my_string.split(""));
+        List<Integer> s2 = new ArrayList<>();
+
+        for(int i = 0; i<s1.size(); i++){
+            char ch = my_string.charAt(i);
+            if(Character.isDigit(ch)){
+                s2.add(Integer.valueOf(s1.get(i)));
+            }
+        }
+
+        Collections.sort(s2);
+        return s2;
+    }
+
+}
