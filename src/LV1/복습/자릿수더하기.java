7109ffaa1f4544f7959125dd06f8b8dee8a380e3
승엽 <tmdduqflfl@naver.com>
2024-08-16 19:33:31 +0900

    8/16 코테 정답맞는데 왜아노대;

--- src/LV1/복습/자릿수더하기.java
+++ src/LV1/복습/자릿수더하기.java
@@ -1,15 +1,29 @@
 package LV1.복습;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 자릿수더하기 {
-    public int solution(int n) {
+
+    // 981
+    public int solution(String n) {
         int answer = 0;
-        String str=Integer.toString(n);
-        int[] digit= new int[str.length()];
-        for(int i=0; i<str.length(); i++){
-            digit[i]=str.charAt(i)-'0';
-            answer+=digit[i];
+        String[] b=n.split("");
+        List<Integer> a = new ArrayList<>();
+        for (String string : b) {
+            a.add(Integer.valueOf(string));
+        }
+
+        for(int i = 0; i<a.size();i++){
+            answer += a.get(i);
         }
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+        자릿수더하기 c = new 자릿수더하기();
+        int sum =c.solution("123");
+        System.out.println(sum);
     }
 }
