ef44cb6232a3da7308800a696a6f05db6a4af13b
승엽 <tmdduqflfl@naver.com>
2024-08-25 15:33:50 +0900

    8/25 최솟값만들기

---
+++ src/LV2/최솟값만들기.java
@@ -0,0 +1,20 @@
+package LV2;
+
+import java.util.Arrays;
+
+public class 최솟값만들기 {
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i=0; i<A.length; i++) {
+            answer += A[i] * B[B.length-i-1];
+        }
+
+        return answer;
+    }
+
+}
