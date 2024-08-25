40ed828e7b3be51785f05fd9f4a0e05790e023f2
승엽 <tmdduqflfl@naver.com>
2024-08-25 15:34:03 +0900

    8/25 피로도

--- src/LV2/피로도.java
+++ src/LV2/피로도.java
@@ -1,16 +1,22 @@
 package LV2;
 
 public class 피로도 {
+    static int answer = 0;
+    static boolean[] visited;
+
     public int solution(int k, int[][] dungeons) {
-        int answer = 0;
-
-        for (int i = 0; i < dungeons.length; i++) {
-            if (dungeons[i][0] > k) {
-                answer++;
-                k=k-dungeons[i][1];
-            }
-        }
-
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
         return answer;
+    }
+
+    public void dfs(int cnt, int fatigue, int[][] dg) {
+        for(int i = 0; i < dg.length; i++){
+            if(visited[i] || dg[i][0] > fatigue) continue;
+            visited[i] = true;
+            dfs(cnt+1, fatigue-dg[i][1], dg);
+            visited[i] = false;
+        }
+        answer = Math.max(cnt, answer);
     }
 }
