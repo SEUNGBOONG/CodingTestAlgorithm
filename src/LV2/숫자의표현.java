397b65b1d32f19145c2a141b66147c6072dc44e9
승엽 <tmdduqflfl@naver.com>
2024-09-04 20:15:50 +0900

    9/4 숫자의 표현 다시풀어보기

---
+++ src/LV2/숫자의표현.java
@@ -0,0 +1,26 @@
+package LV2;
+
+public class 숫자의표현 {
+    public int solution(int n) {
+        int answer = 0;
+        int start = 1;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+            while (sum > n) {
+                sum -= start;
+                start++;
+            }
+            if (sum == n) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        숫자의표현 a = new 숫자의표현();
+        a.solution(15);
+        System.out.println(a.solution(15));
+    }
+}
