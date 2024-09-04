8c05fcb430371718f112188b17b9cd92b74b898e
승엽 <tmdduqflfl@naver.com>
2024-09-04 15:54:27 +0900

    9/4 삼진법 뒤집기 복습해야함

---
+++ src/LV1/삼진법뒤집기.java
@@ -0,0 +1,27 @@
+package LV1;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class 삼진법뒤집기 {
+    public int solution(int n) {
+        int answer = 0;
+        int num1= 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for(){
+            if(n%3==0){
+                queue.add(0);
+                n/=3;
+            }
+            else{
+                num1=n%3;
+                n/=3;
+                queue.add(num1);
+            }
+
+        }
+        return answer;
+    }
+
+}
