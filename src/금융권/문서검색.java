531cf6dc51b0db6ccc5999db5a773cc441bc5b8f
승엽 <tmdduqflfl@naver.com>
2024-09-05 15:11:31 +0900

    9/5 문서검색

---
+++ src/금융권/문서검색.java
@@ -0,0 +1,22 @@
+package 금융권;
+
+import LV1.문자열내P와Y개수;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 문서검색 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String str, String str2){
+        int answer = 0;
+        str.split(str2);
+        List<String> answer22 = new ArrayList<>();
+        answer22.add(str);
+
+        return answer;
+
+    }
+}
