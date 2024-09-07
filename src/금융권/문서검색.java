36cca1c97b53f3c56e17de1fe4b13d23e0337476
승엽 <tmdduqflfl@naver.com>
2024-09-07 16:41:00 +0900

    9/7 문서검색 복습 요망

--- src/금융권/문서검색.java
+++ src/금융권/문서검색.java
@@ -1,20 +1,22 @@
 package 금융권;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Scanner;
 
 public class 문서검색 {
-    public static void main(String[] args) {
 
+    public static long countChar(String str, char ch) {
+        return str.chars()
+                .filter(c -> c == ch)
+                .count();
     }
 
-    public int solution(String str, String str2){
-        int answer = 0;
-        str.split(str2);
-        List<String> answer22 = new ArrayList<>();
-        answer22.add(str);
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
 
-        return answer;
-
+        String str3 = str1.replaceAll(str2, "*");
+        System.out.println(countChar(str3, '*'));
     }
 }
