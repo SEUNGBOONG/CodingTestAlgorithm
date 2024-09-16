ffe7972506a9a87c285fd5ca9f826536712cf299
승엽 <tmdduqflfl@naver.com>
2024-09-16 21:12:58 +0900

    9/16 폰켓몬

---
+++ src/기능별/해시/폰켓몬.java
@@ -0,0 +1,17 @@
+package 기능별.해시;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 폰켓몬 {
+    public int solution(int[] nums) {
+        List<Integer> answer = new ArrayList<>();
+        for (int num : nums) {
+            if (!answer.contains(num)) {
+                answer.add(num);
+            }
+        }
+
+        return Math.min(nums.length / 2, answer.size());
+    }
+}
