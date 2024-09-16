ee27264c06f34acea542d395a922b9c92e6673de
승엽 <tmdduqflfl@naver.com>
2024-09-16 21:12:47 +0900

    9/16 가장 큰수

---
+++ src/기능별/정렬/가장큰수2.java
@@ -0,0 +1,26 @@
+package 기능별.정렬;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class 가장큰수2 {
+
+    public static String solution(int[] numbers) {
+
+        String[] str = new String[numbers.length];
+
+        for(int i = 0; i < numbers.length; i++){
+            str[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(str, (o1, o2) -> (o2 + o1)
+                .compareTo(o1 + o2));
+        if(str[0].equals("0")){
+            return "0";
+        }
+        return String.join("",str);
+    }
+}
