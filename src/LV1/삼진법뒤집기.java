d72380b6963143f9c06b2be5dfbb1336b1130c48
승엽 <tmdduqflfl@naver.com>
2024-09-25 14:35:16 +0900

    9/25 삼진법

--- src/LV1/삼진법뒤집기.java
+++ src/LV1/삼진법뒤집기.java
@@ -12,10 +12,8 @@ public class 삼진법뒤집기 {
             n /= 3;
         }
 
-        // 앞 뒤 반전
         a = new StringBuilder(a).reverse().toString();
 
-        // 3진법 -> 10진법
         return Integer.parseInt(a,3);
     }
 }
