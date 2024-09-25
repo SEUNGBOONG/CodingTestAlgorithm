6f7e7294518ba1de3e482058401ca0c8a7ab2a77
승엽 <tmdduqflfl@naver.com>
2024-09-25 14:35:11 +0900

    9/25 대표값

---
+++ src/백준알고리즘/대표값2.java
@@ -0,0 +1,26 @@
+package 백준알고리즘;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 대표값2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        int mid = 0;
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        int avg = sum / 5;
+        mid = arr[2];
+        System.out.print(avg + "\n" + mid);
+    }
+}
