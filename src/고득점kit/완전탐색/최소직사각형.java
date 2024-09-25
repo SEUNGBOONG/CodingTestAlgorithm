c43f4552b687fe5ce25a8a7602e29c5431f46d71
승엽 <tmdduqflfl@naver.com>
2024-09-25 14:35:35 +0900

    9/25 최소직사각형

---
+++ src/고득점kit/완전탐색/최소직사각형.java
@@ -0,0 +1,21 @@
+package 고득점kit.완전탐색;
+
+public class 최소직사각형 {
+    public int solution(int[][] sizes) {
+
+        int max_row = 0;
+        int max_col = 0;
+
+        for(int i=0;i<sizes.length;i++){
+            if(sizes[i][0]<sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+            if(max_row<sizes[i][0]) max_row = sizes[i][0];
+            if(max_col<sizes[i][1]) max_col = sizes[i][1];
+        }
+
+        return max_col*max_row;
+    }
+}
