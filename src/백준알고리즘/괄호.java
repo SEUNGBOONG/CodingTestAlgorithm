167db25248351a3712bb845e4cd84b456e4e9c68
승엽 <tmdduqflfl@naver.com>
2024-09-25 14:35:05 +0900

    9/25 괄호

--- src/백준알고리즘/괄호.java
+++ src/백준알고리즘/괄호.java
@@ -1,5 +1,43 @@
 package 백준알고리즘;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class 괄호 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < a; i++) {
+            sb.append(stack(br.readLine())).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static String stack(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return "NO";
+            } else {
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
 
 }
