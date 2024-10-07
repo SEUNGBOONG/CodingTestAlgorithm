7ec78463b23abde401ba6278b44b718e745023c1
승엽 <tmdduqflfl@naver.com>
2024-10-07 11:32:09 +0900

    10/7일

---
+++ src/LV2/구명보트.java
@@ -0,0 +1,26 @@
+package LV2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class 구명보트 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+
+        for (int i = people.length - 1; i >= index; i--) {
+            if (people[i] + people[index] <= limit) {
+                index++;
+                answer++;
+            }
+            else {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+
+}
