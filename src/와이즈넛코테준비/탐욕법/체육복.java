0a9069c03befa93e4c1002cbb92957513ed3206c
승엽 <tmdduqflfl@naver.com>
2025-03-30 19:53:21 +0900

    3/30 체육복

---
+++ src/와이즈넛코테준비/탐욕법/체육복.java
@@ -0,0 +1,39 @@
+package 와이즈넛코테준비.탐욕법;
+
+import java.util.Arrays;
+
+public class 체육복 {
+
+    class Solution {
+        public int solution(int n, int[] lost, int[] reserve) {
+            int answer = n - lost.length;
+
+            Arrays.sort(lost);
+            Arrays.sort(reserve);
+
+            //여벌 체육복 있는애가 도둑맞은 경우
+            for(int i = 0; i < lost.length; i++) {
+                for(int j = 0; j < reserve.length; j++) {
+                    if(lost[i] == reserve[j]) {
+                        answer++;
+                        lost[i] = -1;
+                        reserve[j] = -1;
+                        break;
+                    }
+                }
+            }
+
+            //자기번호 +1, -1인 번호에게만 대여가능
+            for(int i = 0; i < lost.length; i++) {
+                for(int j = 0; j < reserve.length;j++) {
+                    if((lost[i]-1 == reserve[j]) || (lost[i]+1 == reserve[j])) {
+                        answer++;
+                        reserve[j] = -1;
+                        break;
+                    }
+                }
+            }
+
+            return answer;
+        }}
+}
