42fc875c76bbe67404de414d1aed68cf8c790f56
승엽 <tmdduqflfl@naver.com>
2025-04-10 00:23:08 +0900

    4/9 코테d

---
+++ src/이카운트/ㅁ.java
@@ -0,0 +1,13 @@
+package 이카운트;
+
+public class ㅁ {
+
+    public static void main(String[] args) {
+        int a =10;
+        int b=10;
+
+        System.out.println(a==b);
+        System.out.println(a);
+
+    }
+}
