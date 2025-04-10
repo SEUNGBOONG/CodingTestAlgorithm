5cea253387be85876484408c1723e2ca07a6cf58
승엽 <tmdduqflfl@naver.com>
2025-04-11 02:59:45 +0900

    4/11 코테

--- src/LV1/푸드파이트.java
+++ src/LV1/푸드파이트.java
@@ -6,17 +6,20 @@ import java.util.List;
 public class 푸드파이트 {
     public String solution(int[] food) {
         String answer = "";
+
         for(int i =0; i<food.length;i++){
             if(food[i]%2!=0){
                 food[i]-=1;
             }
         }
+
         List<String> list =new ArrayList<>();
         for(int i=1;i<food.length/2;i++) {
             for (int j =0; j<food[i];j++){
                 list.add(String.valueOf(food[i]));
             }
         }
+
         for(int i=1;i<food.length/2;i++) {
             for (int j =0; j<food[i];j++){
                 list.add(String.valueOf(food[i]));
