4b2a8e3ec5027f54223fe349e2342c2fa5ab528a
승엽 <tmdduqflfl@naver.com>
2025-04-12 15:49:44 +0900

    4/12 코테

---
+++ src/LV1/가장가까운같은글자.java
@@ -0,0 +1,24 @@
+package LV1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 가장가까운같은글자 {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        Map<Character, Integer> lastIndexMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!lastIndexMap.containsKey(ch)) {
+                answer[i] = -1;
+            } else {
+                answer[i] = i - lastIndexMap.get(ch);
+            }
+            lastIndexMap.put(ch, i); // 현재 위치를 기록
+        }
+
+        return answer;
+    }
+
+}
