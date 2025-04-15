de94b112dbe9e46c33c2ad9572c4f6b874ddd011
승엽 <tmdduqflfl@naver.com>
2025-04-15 15:44:05 +0900

    4/15 ㅈㅊㄱ

---
+++ src/LV1/main.java
@@ -0,0 +1,43 @@
+package LV1;
+
+class Connection {
+
+    private static Connection _inst = null;
+    private int count = 0;
+
+    static public Connection get() {
+        if(_inst == null) {
+            _inst = new Connection();
+            return _inst;
+        }
+        return _inst;
+    }
+
+    public void count() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Connection conn1 = Connection.get();
+        conn1.count();
+
+        Connection conn2 = Connection.get();
+        conn2.count();
+
+        Connection conn3 = Connection.get();
+        conn3.count();
+
+        conn1.count();
+        System.out.print(conn1.getCount());
+    }
+
+}
