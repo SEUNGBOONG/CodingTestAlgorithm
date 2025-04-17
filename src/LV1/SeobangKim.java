53906063f3ea771ba4606264caebcccc636a9cef
승엽 <tmdduqflfl@naver.com>
2025-04-17 17:01:45 +0900

    4/17 ㅈㅊㄱ!

--- src/LV1/SeobangKim.java
+++ src/LV1/SeobangKim.java
@@ -2,13 +2,14 @@ package LV1;
 
 public class SeobangKim {
     public String solution(String[] seoul) {
-        int i=0;
-        for(String s: seoul){
-            if(s.equals("kim")){
+        int i = 0;
+        for (String s : seoul) {
+            if (s.equals("kim")) {
                 break;
             } else
-                i+=1;
+                i += 1;
         }
+
         return String.format("김서방은 %d에 있다", i);
     }
 }
