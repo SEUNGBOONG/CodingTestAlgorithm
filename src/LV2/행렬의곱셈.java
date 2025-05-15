e9623147d345b3c241cf9a26c61b90a83a69f5b8
승엽 <tmdduqflfl@naver.com>
2025-05-15 12:00:00 +0900

    2025년 5월 15일 목요일 커밋

--- src/LV2/행렬의곱셈.java
+++ src/LV2/행렬의곱셈.java
@@ -2,8 +2,8 @@ package LV2;
 
 public class 행렬의곱셈 {
     class Solution {
-        public int[][] solution(int[][] arr1, int[][] arr2) {
-            int row = arr1.length;
+//        public int[][] solution(int[][] arr1, int[][] arr2) {
+     /*       int row = arr1.length;
             int col = arr2[0].length;
             int inner = arr2.length;
 
@@ -19,6 +19,6 @@ public class 행렬의곱셈 {
 
             return answer;
         }
-    }
+    }*/
 
 }
