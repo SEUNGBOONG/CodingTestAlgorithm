455e7f7cc3e9ca2b50459693af278c2a1d7f9b30
승엽 <tmdduqflfl@naver.com>
2025-07-14 16:10:56 +0900

    7/14

---
+++ src/basic/aadada.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class aadada {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a++ + ++a;
+        System.out.print(a + ":" + b);
+    }
+}
