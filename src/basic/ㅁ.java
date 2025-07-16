1119300e7b7f991c6cfe25e3d433a74d908d3300
승엽 <tmdduqflfl@naver.com>
2025-07-16 16:19:09 +0900

    7/16

---
+++ src/basic/ㅁ.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class ㅁ {
+
+    public static void main(String[] args) {
+        int[] data = {3, 5, 8, 12, 17};
+        System.out.println(func(data, 0, data.length - 1));
+    }
+
+    static int func(int[] a, int st, int end) {
+        if (st >= end) return 0;
+        int mid = (st + end) / 2;
+        return a[mid] + Math.max(func(a, st, mid), func(a, mid + 1, end));
+    }
+
+
+}
